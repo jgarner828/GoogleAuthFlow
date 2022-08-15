cdca5430408ca8342978fe03f4a578f76fbcbba0
Justin Garner <j.garner828@gmail.com>
2022-08-15 14:06:06 -0400

    first commit

---
+++ src/main/java/com/garnerj/OAuthGoogle/OAuthGoogleApplication.java
@@ -0,0 +1,13 @@
+package com.garnerj.OAuthGoogle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OAuthGoogleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OAuthGoogleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/garnerj/OAuthGoogle/OAuthGoogleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.garnerj.OAuthGoogle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OAuthGoogleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
